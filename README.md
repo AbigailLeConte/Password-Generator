b9f81dabbcf905c4f81c7d9a55dc0d38b50c0896
Abigail LeConte <113259320+AbigailLeConte@users.noreply.github.com>
2022-11-23 11:42:11 -0500

    var password code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Password-Generator
+
+Password Generator using HTML and CSS powered by JavaScript code.
+
+## Mock-Up
+
+The following animation demonstrates the application functionality:
+
+![A user clicks through an interactive coding quiz, then enters initials to save the high score before resetting and starting over.](./Assets/Mock-up.jpeg)
--- script.js
+++ script.js
@@ -3,7 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(
+    var specialChracters = ! "#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+    var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789".split(""),
+    if (alphabetNumericCharacters.includes(passwordText)) {
+      var letterGuessed = event.writePassword;
+      checkLetters(letterGuessed)
+      checkWin();
+  );
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
